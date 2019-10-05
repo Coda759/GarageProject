400723f64a01aa83f37f6ceb9679f9a9016d78a5
Coda759 <45927826+Coda759@users.noreply.github.com>
2019-10-05 17:12:48 -0400

    Add files via upload

---
+++ ParkingGarage/src/application/ParkingLot.java
@@ -0,0 +1,40 @@
+import java.util.PriorityQueue;
+
+public class ParkingLot {
+	
+	private PriorityQueue<ParkingSpot.MotorbikeSpot> motoLot = new PriorityQueue<ParkingSpot.MotorbikeSpot>();
+	private PriorityQueue<ParkingSpot.SedanSpot> sedanLot = new PriorityQueue<ParkingSpot.SedanSpot>();
+	private PriorityQueue<ParkingSpot.TruckSpot> truckLot = new PriorityQueue<ParkingSpot.TruckSpot>();
+	private final int motoLotSize = 16;
+	private final int truckLotSize = 16;
+	private final int sedanLotSize = 71;
+	private ParkingSpot spot;
+	
+	public void createLot() {
+		for(int i = 1; i < motoLotSize; i++) {
+			ParkingSpot.MotorbikeSpot motoSpot = new ParkingSpot.MotorbikeSpot("M-"+i);
+			motoLot.add(motoSpot);
+		}
+		for(int i = 1; i < truckLotSize; i++) {
+			ParkingSpot.TruckSpot truckSpot = new ParkingSpot.TruckSpot("T-"+i);
+			truckLot.add(truckSpot);
+		}
+		for(int i = 1; i < sedanLotSize; i++) {
+			ParkingSpot.SedanSpot sedanSpot = new ParkingSpot.SedanSpot("S-"+i);
+			sedanLot.add(sedanSpot);
+		}
+	}
+	
+	public String showLot() {
+		return motoLot + "\n" + sedanLot + "\n" + truckLot;
+	}
+	
+	public void parkVehicle(Vehicle vehicle) {
+		switch(spot.getParkingSpotValue()) {
+		
+		}
+	}
+	
+	//TODO: Add parkVehicle, FreeSpot and generate ticket methods
+	
+}
---
+++ ParkingGarage/src/application/Vehicle.java
@@ -0,0 +1,105 @@
+import java.util.Random;
+
+public class Vehicle {
+	
+	enum VehicleType {
+		MOTORBIKE(1), SEDAN(2), TRUCK(3);
+		
+		private int value;
+		
+		private VehicleType(int value) {
+			this.value = value;
+		}
+		
+		public int getSize() {
+			return value;
+		}
+	}
+	 
+	private String[] states = {
+		 "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", 
+		 "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI",
+		 "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC",
+		 "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
+		 "VT", "VA", "WA", "WV", "WI", "WY" }; 
+	private String characters = "ABCDEFGHIGKMNLOPQRSTUVWXYZ";
+	private String numbers = "0123456789";
+	private String licensePlateP1 = "";
+	private String licensePlateP2 = "";
+	private String licensePlateNum = "";
+	private int charLength = 3;
+	private int numLength = 4;
+	private Random rand = new Random();
+	private int size;
+	
+	public String generateLicensePlate() {
+		
+		char[] plate1 = new char[charLength];
+		
+		for(int i = 0; i < charLength; i++) {
+			plate1[i] = characters.charAt(rand.nextInt(characters.length()));
+		}
+		
+		for(int i = 0; i < plate1.length; i++) {
+			licensePlateP1 += plate1[i];
+		}
+		
+		char[] plate2 = new char[numLength];
+		
+		for(int i = 0; i < numLength; i++) {
+			plate2[i] = numbers.charAt(rand.nextInt(numbers.length()));
+		}
+		
+		for(int i = 0; i < plate2.length; i++) {
+			licensePlateP2 += plate2[i];
+		}
+		
+		int randomStateNum = rand.nextInt(states.length);
+		String randomState = states[randomStateNum];
+		
+		setLicensePlate(randomState + " " + licensePlateP1 + "-" + licensePlateP2);
+		
+		return licensePlateNum;
+		
+	}
+
+	public String getLicensePlate() {
+		return licensePlateNum;
+	}
+
+	public void setLicensePlate(String licensePlateNum) {
+		this.licensePlateNum = licensePlateNum;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	//Types of Vehicles:
+	
+	public class Truck extends Vehicle {
+		public Truck() {
+			setLicensePlate(generateLicensePlate());
+			setSize(VehicleType.TRUCK.getSize());
+		}
+	}
+	
+	public class Sedan extends Vehicle {
+		public Sedan() {
+			setLicensePlate(generateLicensePlate());
+			setSize(VehicleType.SEDAN.getSize());
+		}
+	}
+	
+	public class Motorbike extends Vehicle {
+		public Motorbike() {
+			setLicensePlate(generateLicensePlate());
+			setSize(VehicleType.MOTORBIKE.getSize());
+		} 
+	}
+} 
+ 
