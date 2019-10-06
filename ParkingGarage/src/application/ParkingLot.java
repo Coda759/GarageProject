99e56beae8fd0a9733b24432b502cde993422e60
Coda759 <45927826+Coda759@users.noreply.github.com>
2019-10-06 17:03:45 -0400

    Add files via upload

--- ParkingGarage/src/application/ParkingLot.java
+++ ParkingGarage/src/application/ParkingLot.java
@@ -1,4 +1,3 @@
-package application;
 import java.util.PriorityQueue;
 
 public class ParkingLot {
@@ -6,36 +5,61 @@ public class ParkingLot {
 	private PriorityQueue<ParkingSpot.MotorbikeSpot> motoLot = new PriorityQueue<ParkingSpot.MotorbikeSpot>();
 	private PriorityQueue<ParkingSpot.SedanSpot> sedanLot = new PriorityQueue<ParkingSpot.SedanSpot>();
 	private PriorityQueue<ParkingSpot.TruckSpot> truckLot = new PriorityQueue<ParkingSpot.TruckSpot>();
-	private final int motoLotSize = 16;
-	private final int truckLotSize = 16;
-	private final int sedanLotSize = 71;
-	private ParkingSpot spot;
-	
-	public void createLot() {
-		for(int i = 1; i < motoLotSize; i++) {
-			ParkingSpot.MotorbikeSpot motoSpot = new ParkingSpot.MotorbikeSpot("M-"+i);
-			motoLot.add(motoSpot);
-		}
-		for(int i = 1; i < truckLotSize; i++) {
-			ParkingSpot.TruckSpot truckSpot = new ParkingSpot.TruckSpot("T-"+i);
-			truckLot.add(truckSpot);
-		}
-		for(int i = 1; i < sedanLotSize; i++) {
-			ParkingSpot.SedanSpot sedanSpot = new ParkingSpot.SedanSpot("S-"+i);
-			sedanLot.add(sedanSpot);
-		}
-	}
+	private ParkingSpot.MotorbikeSpot motoSpot;
+	private ParkingSpot.SedanSpot sedanSpot;
+	private ParkingSpot.TruckSpot truckSpot;
+	private int motoCount = 0;
+	private int sedanCount = 0;
+	private int truckCount = 0;
+	private final int maxMotorbikeSpots = 15;
+	private final int maxSedanSpots = 70;
+	private final int maxTruckSpots = 15;
 	
 	public String showLot() {
 		return motoLot + "\n" + sedanLot + "\n" + truckLot;
 	}
 	
-	public void parkVehicle(Vehicle vehicle) {
-		switch(spot.getParkingSpotValue()) {
-		
-		}
+	//Optional: show lots separately
+	public String showMotoLot() {
+		return "Vehicles parked in motorbike lot: " + motoLot;
 	}
 	
-	//TODO: Add parkVehicle, FreeSpot and generate ticket methods
+	public String showSedanLot() {
+		return "Vehicles parked in sedan lot: " + sedanLot;
+	}
+	
+	public String showTruckLot() {
+		return "Vehicles parked in truck lot: " + truckLot;
+	}
+	
+	public void parkVehicle(Vehicle vehicle) {		
+		switch(vehicle.getSize()) {
+			case 1: if(motoCount < maxMotorbikeSpots) {
+						motoSpot = new ParkingSpot.MotorbikeSpot("M-" + (motoCount++ +1));
+						motoSpot.addVehicle(vehicle);
+						motoLot.add(motoSpot);
+					} else {
+						System.out.println("No more morbike spots available");
+					}
+					break;
+			case 2: if(sedanCount < maxSedanSpots) {
+						sedanSpot = new ParkingSpot.SedanSpot("S-" + (sedanCount++ +1));
+						sedanSpot.addVehicle(vehicle);
+						sedanLot.add(sedanSpot);
+					} else {
+						System.out.println("No more sedan spots available");
+					}
+					break;
+			case 3: if(truckCount < maxTruckSpots) {
+						truckSpot = new ParkingSpot.TruckSpot("T-" + (truckCount++ +1));
+						truckSpot.addVehicle(vehicle);
+						truckLot.add(truckSpot);
+					} else {
+						System.out.println("No more truck spots available");
+					}
+					break;
+					default: System.out.println("Error, this type of car is not accepted");
+		}
+	}
 	
 }
